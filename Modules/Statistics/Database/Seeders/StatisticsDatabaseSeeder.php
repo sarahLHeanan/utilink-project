ee816e20413309dcde8d4ddcab25fb7b8b067b30
Sarah Heanan <sarahlheanan@gmail.com>
2024-03-11 13:55:17 +0000

    Autoload modules, create statistics module

---
+++ Modules/Statistics/Database/Seeders/StatisticsDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Statistics\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StatisticsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Statistics/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('statistics::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('statistics.name') !!}</p>
+@endsection
